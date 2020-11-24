5ca2ca35e38dba43f37a04a2002b1d6c391a6616
tanopanta <rainstorm2525@gmail.com>
2020-11-24 23:27:59 +0900

    add pattern function

--- hello.erl
+++ hello.erl
@@ -1,6 +1,11 @@
 -module(hello).
 -export([hello_world/0]).
+-export([echo/1]).
 
 hello_world() ->
     Msg = "Hello, World!",
     io:format("~s~n", [Msg]).
+
+echo(a) -> "a";
+echo(b) -> "b";
+echo(_) -> error.
